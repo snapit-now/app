307a76bf76f00016963057fcdfca3bf59ace5d5f
snapit-now <dailydealsclub365@gmail.com>
2025-09-23 20:15:25 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,18 +5,6 @@ window.onpopstate = function () {
 };
 
 
-window.addEventListener('load', function() {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/firebase-messaging-sw.js')
-      .then(function(registration) {
-        console.log('Registration successful, scope is:', registration.scope);
-      }).catch(function(err) {
-        console.log('Service worker registration failed, error:', err);
-      });
-  }
-});
-
-
 // Visitor
  async function getVisitorInfo() {
       try {
@@ -318,3 +306,4 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
